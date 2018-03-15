3560e4ddcaf6343f9dbdcf9c0d3ae8ea9f00126c
smflanag <smflanag@gmail.com>
2018-03-15 21:13:35 +0000

    topic join/leave working

--- Aggregator/settings.py
+++ Aggregator/settings.py
@@ -123,3 +123,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
--- groups/urls.py
+++ groups/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'topic/$', group_views.TopicList.as_view(), name='topic_list'),
     url(r'^topic/(?P<slug>[-\w]+)/$', group_views.TopicDetails.as_view(), name='topic_detail'),
     url(r'topic_create/$', group_views.TopicCreate.as_view(), name='new_topic'),
+    url(r'join/(?P<slug>[-\w]+)/$',group_views.JoinTopic.as_view(),name='join'),
+    url(r'leave/(?P<slug>[-\w]+)/$',group_views.LeaveTopic.as_view(),name='leave'),
     ]
