42f3b2abc156f5c39a5788a92646c7d17225bb3d
smflanag <smflanag@gmail.com>
2018-11-15 13:46:34 +0000

    javascript comments code moved to .js file

--- articles/static/articles/main.js
+++ articles/static/articles/main.js
@@ -17,7 +17,50 @@ $(document).ready(function(){
                 $("#i-arrow-bottom").css('color','green');
             });
         });
+    $("#comment").click(function(){
+        $.ajax(
+            {
+                type: "POST",
+                url: "/articles/"+window.article_id+"/comment",
+                data: JSON.stringify({
+                    commenter:window.user_id,
+                    article: window.article_id,
+                    comment_body:$("#id_comment_body").val()
 
-
-
+                }),
+                success: function(response){
+                    console.log(5);
+                    var comment_body = $("#id_comment_body").val();
+                    var d = new Date();
+                    var hours = d.getHours()
+                    var newHours = 0
+                    var ampm = ""
+                    if (hours<13) {
+                        newHours = hours;
+                        ampm = " a.m.";
+                        } else if (hours === 0) {
+                        newHours = 12;
+                        ampm = " a.m.";
+                        } else {
+                        newHours = hours-12;
+                        ampm = " p.m.";}
+                    var month = d.getMonth()
+                    var monthNames = [
+                        "Jan", "Feb", "Mar",
+                        "Apr", "May", "Jun", "Jul",
+                        "Aug", "Sep", "Oct",
+                        "Nov", "Dec"
+                        ];
+                    var newMonth = monthNames[month]
+                    var datestring = newMonth+". "+d.getDate()+", "+d.getFullYear()+", "+
+                    newHours + ":" + d.getMinutes()+ampm
+                    var output = $("<div class=\"article_details\"><h5>"+comment_body+"</h5>"+"<h6>Created by: "
+                    +window.user+"</h6><h6>Posted at: "+datestring+"</h6></div>");
+                    $('.comment_list').prepend(output);
+                },
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+             }
+             );
+        });
     });
